04b00a13af9e858263a791bc4521c2fca160ddb4
xpakx <55586470+xpakx@users.noreply.github.com>
2024-03-27 17:26:13 +0100

    feat(gateway): pass request to downstream service

--- gateway/src/main.rs
+++ gateway/src/main.rs
@@ -1,4 +1,5 @@
-use axum::{Router, routing::get};
+use axum::{Router, routing::get, extract::{Request, State}, body::{Body, to_bytes}};
+use reqwest::Client;
 use tracing::{info, debug};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
@@ -17,6 +18,7 @@ async fn main() {
 
     let app = Router::new()
         .route("/", get(index))
+        .route("/game/*path", get(game))
         .with_state(client);
 
     info!("Initializing router…");
@@ -35,4 +37,24 @@ async fn main() {
 async fn index() -> String {
     debug!("Index requested.");
     return String::from("Hello, world!")
+}
+
+async fn game(State(client): State<Client>, req: Request<Body>) -> String {
+    debug!("{} matched against 'game/**'.", req.uri());
+    let target = "http://localhost";
+    let target_port = 8081;
+    let uri = format!("{}:{}{}", target, target_port, req.uri().path());
+    debug!("Calling {}.", uri);
+    let (parts, body) = req.into_parts();
+
+    let response = client.request(parts.method, uri)
+        .version(parts.version)
+        .headers(parts.headers)
+        .body(to_bytes(body, usize::MAX).await.unwrap())
+        .send()
+        .await;
+
+    let result = response.unwrap().text().await.unwrap();
+
+    return String::from(result)
 }
