a845facb5e01a3b5f0a9f892b62ab23c79352443
xpakx <55586470+xpakx@users.noreply.github.com>
2024-03-27 16:08:20 +0100

    feat(gateway): create axum router

--- gateway/src/main.rs
+++ gateway/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let client = reqwest::Client::new();
+
+    let app = Router::new()
+        .route("/", get(index))
+        .with_state(client);
+
+    let host = "0.0.0.0";
+    let port = 3000;
+    let listener = tokio::net::TcpListener::bind(format!("{}:{}", host, port))
+        .await
+        .unwrap();
+
+    axum::serve(listener, app)
+        .await
+        .unwrap();
+}
+
+async fn index() -> String {
+    return String::from("Hello, world!")
 }
