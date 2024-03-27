9e23a8273530f1043a49247d45b7643f60716059
xpakx <55586470+xpakx@users.noreply.github.com>
2024-03-27 16:24:18 +0100

    chore(gateway): add logging

--- gateway/src/main.rs
+++ gateway/src/main.rs
@@ -1,18 +1,31 @@
 use axum::{Router, routing::get};
+use tracing::{info, debug};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "checkers=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     let client = reqwest::Client::new();
+    info!("Created reqwest client.");
 
     let app = Router::new()
         .route("/", get(index))
         .with_state(client);
 
+    info!("Initializing router…");
     let host = "0.0.0.0";
     let port = 3000;
     let listener = tokio::net::TcpListener::bind(format!("{}:{}", host, port))
         .await
         .unwrap();
+    info!("Router initialized. Listening on port {}.", port);
 
     axum::serve(listener, app)
         .await
@@ -20,5 +33,6 @@ async fn main() {
 }
 
 async fn index() -> String {
+    debug!("Index requested.");
     return String::from("Hello, world!")
 }
