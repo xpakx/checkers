ccf250d8856eaa563b0b0d17afd88cb7f14cf987
xpakx <55586470+xpakx@users.noreply.github.com>
2024-03-28 14:35:48 +0100

    feat(main): configure axum and logging

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get, response::IntoResponse};
+use tracing::info;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "checkers=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let app = Router::new()
+        .route("/authenticate", get(handle));
+
+    info!("Initializing router…");
+    let host = "0.0.0.0";
+    let port = 8080;
+    let listener = tokio::net::TcpListener::bind(format!("{}:{}", host, port))
+        .await
+        .unwrap();
+    info!("Router initialized. Listening on port {}.", port);
+
+    axum::serve(listener, app)
+        .await
+        .unwrap();
+}
+
+async fn handle() -> impl IntoResponse {
+    return "Hello world"
 }
