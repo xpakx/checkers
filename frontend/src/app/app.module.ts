3054522f624906b700406b7dcbd08e292cf66f9c
xpakx <55586470+xpakx@users.noreply.github.com>
2024-04-09 16:35:42 +0200

    feat(frontend): intercept unauthorized exceptions and refresh token if necessary

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { ModalRegisterComponent } from './auth/modal-register/modal-register.component';
 import { ModalLoginComponent } from './auth/modal-login/modal-login.component';
+import { ErrorInterceptor } from './error/error.interceptor';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,13 @@ import { ModalLoginComponent } from './auth/modal-login/modal-login.component';
     FormsModule,
     ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
