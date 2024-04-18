a065cd6516a1d8378c37d32b9d311d338519a7e1
xpakx <55586470+xpakx@users.noreply.github.com>
2024-04-18 18:52:45 +0200

    feat(main): process game event

--- main/src/rabbit/game_consumer.rs
+++ main/src/rabbit/game_consumer.rs
@@ -26,11 +26,36 @@ pub fn set_game_delegate(consumer: lapin::Consumer, channel: Channel, state: Arc
                 };
 
                 let message = std::str::from_utf8(&delivery.data).unwrap();
-                // TODO: deserialize
+                let message: GameEvent = match serde_json::from_str(message) {
+                    Ok(msg) => msg,
+                    Err(err) => {
+                        error!("Failed to deserialize move message: {:?}", err);
+                        return;
+                    }
+                };
                 info!("Received message: {:?}", &message);
 
-                // TODO: Process and serialize
-                // TODO: publish response
+                let game = get_game_details(&state.db, &message.game_id).await;
+                
+                // TODO
+                let response = match game {
+                    Err(_) => StateEvent { game_id: message.game_id, error: true, error_message: Some("".into()) },
+                    Ok(game) => StateEvent { game_id: game.id, error: false, error_message: None },
+                };
+                info!("Response: {:?}", &response);
+                let response = serde_json::to_string(&response).unwrap();
+
+                if let Err(err) = channel
+                    .basic_publish(
+                        STATE_EXCHANGE,
+                        "state",
+                        Default::default(),
+                        response.into_bytes().as_slice(),
+                        Default::default(),
+                        )
+                        .await {
+                            error!("Failed to publish message to destination exchange: {:?}", err);
+                        };
 
                 delivery
                     .ack(BasicAckOptions::default())
@@ -40,4 +65,19 @@ pub fn set_game_delegate(consumer: lapin::Consumer, channel: Channel, state: Arc
         }
     }
     );
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct GameEvent {
+    game_id: i64,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct StateEvent {
+    game_id: i64,
+    error: bool,
+    error_message: Option<String>,
+    // TODO
 }
