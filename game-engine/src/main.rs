07fc2febeecbea6d24e796da9adfefb5e169fefb
xpakx <55586470+xpakx@users.noreply.github.com>
2024-04-23 14:58:54 +0200

    feat(engine): create rabbitmq connection pool and connect/reconnect

--- game-engine/src/main.rs
+++ game-engine/src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use std::time::Duration;
+
+#[tokio::main]
+async fn main() {
+    let rabbit_url = "amqp://guest:guest@localhost:5672";
+    let mut cfg = deadpool_lapin::Config::default();
+    cfg.url = Some(rabbit_url.into());
+    let lapin_pool = cfg.create_pool(Some(deadpool_lapin::Runtime::Tokio1)).unwrap();
+    lapin_listen(lapin_pool.clone()).await;
+}
+
+pub async fn lapin_listen(pool: deadpool_lapin::Pool) {
+    let mut retry_interval = tokio::time::interval(Duration::from_secs(5));
+    loop {
+        retry_interval.tick().await;
+        println!("Connecting rmq consumer...");
+        match init_lapin_listen(pool.clone()).await {
+            Ok(_) => println!("RabbitMq listen returned"),
+            Err(e) => println!("RabbitMq listen had an error: {}", e),
+        };
+    }
+}
+
+async fn init_lapin_listen(pool: deadpool_lapin::Pool) -> Result<(), Box<dyn std::error::Error>> {
+    let rmq_con = pool.get().await
+        .map_err(|e| {
+        println!("Could not get RabbitMQ connnection: {}", e);
+        e
+    })?;
+    let channel = rmq_con.create_channel().await?;
+    let mut test_interval = tokio::time::interval(Duration::from_secs(5));
+
+    loop {
+        test_interval.tick().await;
+        match channel.status().connected() {
+            false => break,
+            true => {},
+        }
+    }
+
+    Ok(())
 }
