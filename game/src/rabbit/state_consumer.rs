d09ddfeb49a8cdbe351233313e07c41730211b76
xpakx <55586470+xpakx@users.noreply.github.com>
2024-05-03 16:39:29 +0200

    fix(game): fix board generation for board response

--- game/src/rabbit/state_consumer.rs
+++ game/src/rabbit/state_consumer.rs
@@ -173,7 +173,21 @@ impl GameResponse {
             }
         })
         .collect();
-        let current_state = fields.chunks(size).map(|c| c.to_vec()).collect();
+        let current_state: Vec<Vec<Field>> = fields
+            .chunks(size/2)
+            .enumerate()
+            .map(|(i, chunk)| {
+                if i % 2 == 0 {
+                    chunk.iter()
+                        .flat_map(|c| [Field::Empty, c.clone()])
+                        .collect()
+                } else {
+                    chunk.iter()
+                        .flat_map(|c| [c.clone(), Field::Empty])
+                        .collect()
+                }
+            })
+            .collect();
         GameResponse {
             status: game.status,
             my_turn: false, // TODO
