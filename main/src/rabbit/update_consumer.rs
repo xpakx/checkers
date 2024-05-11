bed92c43539bfdc1a327a1ea0c3aee2ab727eea1
xpakx <55586470+xpakx@users.noreply.github.com>
2024-05-11 19:59:25 +0200

    refactor(main): extract methods for processing event in update consumer

--- main/src/rabbit/update_consumer.rs
+++ main/src/rabbit/update_consumer.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 
-use lapin::{message::DeliveryResult, options::BasicAckOptions};
+use lapin::{message::{Delivery, DeliveryResult}, options::BasicAckOptions};
 use serde::{Deserialize, Serialize};
 use tracing::{info, error};
 
@@ -22,40 +22,8 @@ pub fn set_update_delegate(consumer: lapin::Consumer, state: Arc<AppState>) {
                     }
                 };
 
-                let message = std::str::from_utf8(&delivery.data).unwrap();
-                let message: UpdateEvent = match serde_json::from_str(message) {
-                    Ok(msg) => msg,
-                    Err(err) => {
-                        error!("Failed to deserialize update event: {:?}", err);
-                        return; // TODO
-                    }
-                };
-                info!("Received message: {:?}", &message);
-
-                let game = get_game(&state.db, &message.game_id).await;
-                if let Ok(game) = game {
-                    let game = GameModel {
-                        id: game.id, 
-                        status: match message.status {
-                            GameStatus::NotFinished => repository::GameStatus::NotFinished,
-                            GameStatus::Won => repository::GameStatus::Won,
-                            GameStatus::Lost => repository::GameStatus::Lost,
-                            GameStatus::Drawn => repository::GameStatus::Drawn,
-                        },
-                        current_state: message.current_state.clone(),
-                        user_turn: message.user_turn,
-                        ..Default::default()
-                    };
-                    if let Ok(_) = update_game(&state.db, game).await {
-                        let mv = MoveModel {
-                            game_id: message.game_id,
-                            current_state: message.current_state,
-                            created_at: Some(message.timestamp),
-                            last_move: message.last_move,
-                            ..Default::default()
-                        };
-                        _ = save_move(&state.db, mv)
-                    }
+                if let Ok(update) = get_event_from_message(&delivery) {
+                    process_message(update, state).await;
                 }
 
                 delivery
@@ -82,4 +50,45 @@ struct UpdateEvent {
 #[derive(Debug, Serialize, Deserialize)]
 pub enum GameStatus {
     NotFinished, Won, Lost, Drawn,
+}
+
+fn get_event_from_message(delivery: &Delivery) -> Result<UpdateEvent, ()> {
+    let message = std::str::from_utf8(&delivery.data).unwrap();
+    let message: UpdateEvent = match serde_json::from_str(message) {
+        Ok(msg) => msg,
+        Err(err) => {
+            error!("Failed to deserialize update event: {:?}", err);
+            return Err(());
+        }
+    };
+    info!("Received message: {:?}", &message);
+    return Ok(message);
+}
+
+async fn process_message(message: UpdateEvent, state: Arc<AppState>) {
+    let game = get_game(&state.db, &message.game_id).await;
+    if let Ok(game) = game {
+        let game = GameModel {
+            id: game.id, 
+            status: match message.status {
+                GameStatus::NotFinished => repository::GameStatus::NotFinished,
+                GameStatus::Won => repository::GameStatus::Won,
+                GameStatus::Lost => repository::GameStatus::Lost,
+                GameStatus::Drawn => repository::GameStatus::Drawn,
+            },
+            current_state: message.current_state.clone(),
+            user_turn: message.user_turn,
+            ..Default::default()
+        };
+        if let Ok(_) = update_game(&state.db, game).await {
+            let mv = MoveModel {
+                game_id: message.game_id,
+                current_state: message.current_state,
+                created_at: Some(message.timestamp),
+                last_move: message.last_move,
+                ..Default::default()
+            };
+            _ = save_move(&state.db, mv)
+        }
+    }
 }
